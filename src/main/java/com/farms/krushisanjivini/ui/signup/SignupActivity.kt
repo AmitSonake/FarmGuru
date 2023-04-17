e51e3010f0f767d96fd4bd6bf6f43c9b3c86a4de
Amit <amitsonake143@gmail.com>
2023-04-17 09:52:41 +0530

    App_Version 1.0.1

--- src/main/java/com/farms/krushisanjivini/ui/signup/SignupActivity.kt
+++ src/main/java/com/farms/krushisanjivini/ui/signup/SignupActivity.kt
@@ -20,6 +20,12 @@ import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.text.Editable
+
+import android.text.TextWatcher
+
+
+
 
 
 class SignupActivity : AppCompatActivity() {
@@ -84,6 +90,64 @@ class SignupActivity : AppCompatActivity() {
             }
 
         }
+        binding.otpEditText1.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    binding.otpEditText2.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
+
+        binding.otpEditText2.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    binding.otpEditText3.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
+        binding.otpEditText3.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                if (s.length == 1) {
+                    binding.otpEditText4.requestFocus()
+                }
+            }
+
+            override fun beforeTextChanged(
+                s: CharSequence, start: Int, count: Int,
+                after: Int
+            ) {
+            }
+
+            override fun onTextChanged(
+                s: CharSequence, start: Int, before: Int,
+                count: Int
+            ) {
+            }
+        })
 
     }
 
